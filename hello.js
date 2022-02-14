7fe60db5b1ffa89cedda40c11e7ee68645da2462
Marin Alexandru <74427063+Alfers009@users.noreply.github.com>
2022-02-14 23:10:57 +0200

    14.02

---
+++ hello.js
@@ -0,0 +1,21 @@
+var time =3000
+var hello ="Heleoo mother fukers"
+var adouaalerta="Aceasta este a doua alerta"
+var isLate = true
+var score = 6
+if (isLate) {
+    alert("Go home")
+    
+} else {
+    alert("enjoy")
+    
+}
+
+setTimeout(welcome,time)
+time = time + 5000
+function welcome() {
+    alert(hello)
+    alert(adouaalerta)
+    
+}
+// comentariu in js//
