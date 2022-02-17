268ee7e4d4e933ca25784f2c845b163b26b251c2
Marin Alexandru <74427063+Alfers009@users.noreply.github.com>
2022-02-17 21:21:04 +0200

    16.02

---
+++ genchecker.js
@@ -0,0 +1,10 @@
+year = document.getElementById("year").value
+
+if (year < 1945) {
+    document.getElementById("year").value = "Johnny Bravo";)
+    
+} if else (year > 1945){
+    document.write("Boomer")
+
+    
+}
