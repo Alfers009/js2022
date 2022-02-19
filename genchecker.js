b3912a638d24a7a2e8ebccf1b6be2e18a303354f
Marin Alexandru <74427063+Alfers009@users.noreply.github.com>
2022-02-19 15:33:28 +0200

    Generation Checker Get done

--- genchecker.js
+++ genchecker.js
@@ -1,7 +1,24 @@
-function Generationvalue() {
-    var yearvalue = document.getElementById("year")
-    var yeartext = "Silent"
-if (yeartext == 1945) {
-    document.getElementById("yearbox").innerHTML = yeartext
-}
+function Generation() {
+    var yearvalue = document.getElementById("yearbox").value
+    if (yearvalue <= 1945) {
+        document.getElementById("Final").innerHTML = "Silent"
+        
+    }
+    if (yearvalue <= 1964) {
+        document.getElementById("Final").innerHTML = "Boomer"
+        
+    }
+    else if (yearvalue <= 1980) {
+        document.getElementById("Final").innerHTML = "Generation X"
+        
+    }
+    else if (yearvalue <= 1996) {
+        document.getElementById("Final").innerHTML = "Milenials"
+        
+    }
+    else if (yearvalue <= 2012) {
+        document.getElementById("Final").innerHTML = "Generation Z"
+        
+    }
+    
 }
